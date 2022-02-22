ab95d4c1148c4812477c5a00037fdddab5b9570f
L-tech <legendabrahamonoja@gmail.com>
2022-02-22 02:00:56 +0100

    update: vote on proposals

---
+++ scripts/10-create-vote-proposals.js
@@ -0,0 +1,75 @@
+import { ethers } from "ethers";
+import sdk from "./1-initialize-sdk.js";
+
+// The voting contract.
+const voteModule = sdk.getVoteModule(
+  "0x56ea9a99d9f8b2691c711950bc796aEcAC45Eee2",
+);
+
+// Our ERC-20 contract.
+const tokenModule = sdk.getTokenModule(
+  "0x7c8d0def399CA9264c8D6B9c7EE1F8C955e1B836",
+);
+
+(async () => {
+  try {
+    const amount = 420_000;
+    // Create proposal to mint 420,000 new token to the treasury.
+    await voteModule.propose(
+      "Should Charity DAO mint an additional " + amount + " tokens into the treasury?",
+      [
+        {
+          // Our nativeToken is ETH. nativeTokenValue is the amount of ETH we want
+          // to send in this proposal. In this case, we're sending 0 ETH.
+          // We're just minting new tokens to the treasury. So, set to 0.
+          nativeTokenValue: 0,
+          transactionData: tokenModule.contract.interface.encodeFunctionData(
+            "mint",
+            [
+              voteModule.address,
+              ethers.utils.parseUnits(amount.toString(), 18),
+            ]
+          ),
+          // Our token module that actually executes the mint.
+          toAddress: tokenModule.address,
+        },
+      ]
+    );
+
+    console.log("✅ Successfully created proposal to mint tokens");
+  } catch (error) {
+    console.error("failed to create first proposal", error);
+    process.exit(1);
+  }
+
+  try {
+    const amount = 6_900;
+    // Create proposal to transfer ourselves 6,900 tokens for being awesome.
+    await voteModule.propose(
+      "Should the DAO transfer " +
+      amount + " tokens from the treasury to " +
+      process.env.WALLET_ADDRESS + " for being awesome?",
+      [
+        {
+          // Again, we're sending ourselves 0 ETH. Just sending our own token.
+          nativeTokenValue: 0,
+          transactionData: tokenModule.contract.interface.encodeFunctionData(
+            "transfer",
+            [
+              process.env.WALLET_ADDRESS,
+              ethers.utils.parseUnits(amount.toString(), 18),
+            ]
+          ),
+
+          toAddress: tokenModule.address,
+        },
+      ]
+    );
+
+    console.log(
+      "✅ Successfully created proposal to reward ourselves from the treasury, let's hope people vote for it!"
+    );
+  } catch (error) {
+    console.error("failed to create second proposal", error);
+  }
+})();
