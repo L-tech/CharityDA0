01c3b521abe7f8ecc9a374ad0792c266621ddf05
L-tech <legendabrahamonoja@gmail.com>
2022-02-22 02:14:42 +0100

    update: transfer access

---
+++ scripts/11-revoke-roles.js
@@ -0,0 +1,26 @@
+import sdk from "./1-initialize-sdk.js";
+
+const tokenModule = sdk.getTokenModule(
+  "0x7c8d0def399CA9264c8D6B9c7EE1F8C955e1B836",
+);
+
+(async () => {
+  try {
+    // Log the current roles.
+    console.log(
+      "👀 Roles that exist right now:",
+      await tokenModule.getAllRoleMembers()
+    );
+
+    // Revoke all the superpowers my wallet had over the ERC-20 contract.
+    await tokenModule.revokeAllRolesFromAddress(process.env.WALLET_ADDRESS);
+    console.log(
+      "🎉 Roles after revoking ourselves",
+      await tokenModule.getAllRoleMembers()
+    );
+    console.log("✅ Successfully revoked our superpowers from the ERC-20 contract");
+
+  } catch (error) {
+    console.error("Failed to revoke ourselves from the DAO treasury", error);
+  }
+})();
